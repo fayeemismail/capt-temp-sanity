d532517af54cb3daa850caad2abd089288e5b709
fayeemismail <faheemmuhammed703@gmail.com>
2026-04-17 17:09:19 +0530

    icon setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -69,6 +69,13 @@ export const metadata: Metadata = {
     index: true,
     follow: true,
   },
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-48x48.png', sizes: '48x48', type: 'image/png' },
+      { url: '/favicon-192x192.png', sizes: '192x192', type: 'image/png' },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
